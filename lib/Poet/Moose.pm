a6c9e969e538ecb2d18cdd1eb64445f50d612e9c
Jonathan Swartz <swartz@pobox.com>
2011-09-09 18:31:02 -0700

    add

---
+++ lib/Poet/Moose.pm
@@ -0,0 +1,49 @@
+package Poet::Moose;
+use Moose                      ();
+use MooseX::HasDefaults::RO    ();
+use MooseX::StrictConstructor  ();
+use Method::Signatures::Simple ();
+use Moose::Exporter;
+use strict;
+use warnings;
+Moose::Exporter->setup_import_methods( also => ['Moose'] );
+
+sub init_meta {
+    my $class     = shift;
+    my %params    = @_;
+    my $for_class = $params{for_class};
+    Method::Signatures::Simple->import( into => $for_class );
+    Moose->init_meta(@_);
+    MooseX::StrictConstructor->init_meta(@_);
+    MooseX::HasDefaults::RO->init_meta(@_);
+    {
+        no strict 'refs';
+        *{ $for_class . '::CLASS' } = sub () { $for_class };    # like CLASS.pm
+    }
+}
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Poet::Moose - Poet Moose policies
+
+=head1 SYNOPSIS
+
+    # instead of use Moose;
+    use Poet::Moose;
+
+=head1 DESCRIPTION
+
+Sets certain Moose behaviors for Poet's internal classes. Using this module is
+equivalent to
+
+    use CLASS;
+    use Moose;
+    use MooseX::HasDefaults::RO;
+    use MooseX::StrictConstructor;
+    use Method::Signatures::Simple;
