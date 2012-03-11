4f50addf9119e7968345e44cc8a1e4964239b589
Jonathan Swartz <swartz@pobox.com>
2012-03-11 15:40:17 -0700

    rename Poet::Log::Manager to Poet::Log

--- lib/Poet/Log.pm
+++ lib/Poet/Log.pm
@@ -1,11 +1,88 @@
 package Poet::Log;
-use Log::Any;
+use Poet qw($conf $env);
+use Log::Any::Adapter;
+use Method::Signatures::Simple;
 use strict;
 use warnings;
 
-sub get_logger {
-    my $class = shift;
-    return Log::Any->get_logger(@_);
+method get_logger ($class: %params) {
+    my $category = $params{category} || caller();
+    return Log::Any->get_logger( category => $category );
+}
+
+method initialize_logging ($class:) {
+    $class->_initialize_log4perl;
+}
+
+method _initialize_log4perl ($class:) {
+    require Log::Log4perl;
+    unless ( Log::Log4perl->initialized() ) {
+        my $config_string = $class->_generate_log4perl_config();
+        Log::Log4perl->init( \$config_string );
+        Log::Any::Adapter->set('Log4perl');
+    }
+}
+
+method _generate_log4perl_config ($class:) {
+    my %core_defaults = (
+        level  => 'info',
+        output => 'poet.log',
+        layout => '%d{dd/MMM/yyyy:HH:mm:ss.SS} [%p] %c - %m - %F:%L - %P%n'
+    );
+    my %defaults = ( %core_defaults, %{ $conf->get_hash('log.defaults') } );
+    my %classes = %{ $conf->get_hash('log.class') };
+
+    # must flatten %classes back into dot notation, haha
+
+    foreach my $set ( values(%classes) ) {
+        foreach my $key (qw(level output layout)) {
+            $set->{$key} = $defaults{$key} if !exists( $set->{$key} );
+        }
+    }
+    foreach my $set ( \%defaults, values(%classes) ) {
+        if ( $set->{output} =~ /^(?:stderr|stdout)$/ ) {
+            $set->{appender_class} = "Log::Log4perl::Appender::Screen";
+            $set->{stderr} = 0 if $set->{output} eq 'stdout';
+        }
+        else {
+            $set->{appender_class} = "Log::Log4perl::Appender::File";
+            $set->{filename}       = $set->{output};
+            $set->{filename}       = $env->logs_path( $set->{filename} )
+              if $set->{filename} !~ m|^/|;
+        }
+    }
+    return join(
+        "\n",
+        $class->_generate_lines( 'log4perl.logger', 'default', \%defaults ),
+        map {
+            $class->_generate_lines( "log4perl.logger.$_",
+                $class->_flatten_class_name($_),
+                $classes{$_} )
+          } sort( keys(%classes) ),
+    );
+}
+
+method _generate_lines ($class: $logger, $appender, $set) {
+    my $full_appender = "log4perl.appender.$appender";
+    my @pairs         = (
+        [ $logger => join( ", ", uc( $set->{level} ), $appender ) ],
+        [ $full_appender          => $set->{appender_class} ],
+        [ "$full_appender.layout" => 'Log::Log4perl::Layout::PatternLayout' ],
+        [ "$full_appender.layout.ConversionPattern" => $set->{layout} ]
+    );
+    foreach my $key qw(filename stderr) {
+        if ( exists( $set->{$key} ) ) {
+            push( @pairs, [ "$full_appender.$key" => $set->{$key} ] );
+        }
+    }
+
+    my $lines = join( "\n", map { join( " = ", @$_ ) } @pairs ) . "\n";
+    return $lines;
+}
+
+method _flatten_class_name ($class: $class_name) {
+    $class_name =~ s/(::|\.)/_/g;
+    return $class_name;
 }
 
 1;
@@ -30,7 +107,7 @@ Poet::Log -- Poet logging
     my $log = $m->log;
 
     # For an arbitrary category...
-    my $log = Log::Any->get_logger(category => 'Foo::Bar');
+    my $log = Poet::Log->get_logger(category => 'Foo::Bar');
 
     # then...
     $log->error("an error occurred");
