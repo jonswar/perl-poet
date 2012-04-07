d86d1ed63a82d8511665201485dfff4356c7a744
Jonathan Swartz <swartz@pobox.com>
2012-04-07 13:54:56 -0500

    added

---
+++ lib/Poet/Plack/Request.pm
@@ -0,0 +1,17 @@
+package Poet::Plack::Request;
+use Poet::Moose;
+extends 'Plack::Request';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Poet::Plack::Request - Poet's subclass of Plack::Request
+
+=head1 DESCRIPTION
+
+This is a Poet-specific subclass of Plack::Request, reserved for future
+additions and overrides.
+
---
+++ lib/Poet/Plack/Response.pm
@@ -0,0 +1,17 @@
+package Poet::Plack::Response;
+use Poet::Moose;
+extends 'Plack::Response';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Poet::Plack::Response - Poet's subclass of Plack::Response
+
+=head1 DESCRIPTION
+
+This is a Mason-specific subclass of Plack::Response, reserved for future
+additions and overrides.
+
