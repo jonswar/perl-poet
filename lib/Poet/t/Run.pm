5dd27c1369824c4603a16de4e4c5cd9e27afaec1
Jonathan Swartz <swartz@pobox.com>
2012-04-19 04:26:13 -0700

    check run log

--- lib/Poet/t/Run.pm
+++ lib/Poet/t/Run.pm
@@ -1,5 +1,6 @@
 package Poet::t::Run;
 use Poet::Test::Util;
+use Poet::Util qw(read_file);
 use Guard;
 use IO::Socket;
 use IPC::System::Simple qw(run);
@@ -11,11 +12,18 @@ use base qw(Test::Class);
 
 sub test_run : Tests {
     my $env = temp_env(
-        conf => { layer => 'development', plackup => { port => 9999 } } );
+        conf => { layer => 'development', server => { port => 9999 } } );
     my $root_dir = $env->root_dir;
+    my $run_log  = "$root_dir/logs/run.log";
     if ( my $pid = fork() ) {
         scope_guard { kill( 1, $pid ) };
         sleep(2);
+        ok( -f $run_log, "run log exists" );
+        like(
+            read_file($run_log),
+            qr/Watching .* for file updates.*Accepting connections at .*:9999/s,
+            "run log contents"
+        );
         ok( is_port_active( 9999, '127.0.0.1' ), "port 9999 active" );
 
         my $mech = Test::WWW::Mechanize->new;
@@ -26,7 +34,7 @@ sub test_run : Tests {
     else {
         close STDOUT;
         close STDERR;
-        exec( $env->bin_path("run.pl") );
+        exec( $env->bin_path("run.pl > $run_log 2>&1") );
     }
 }
 
