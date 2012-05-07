ef492a9a75119ac14aa7bfe603494ad7ef3e2e66
Jonathan Swartz <swartz@pobox.com>
2012-05-07 10:17:12 -0700

    add comment

--- eg/blog/bin/install.sh
+++ eg/blog/bin/install.sh
@@ -3,6 +3,7 @@
 # Requires cpanm: http://search.cpan.org/perldoc?App::cpanminus
 cpanm -S --notest DateTime DBD::SQLite Poet Rose::DB::Object
 
+# Load schema into database
 cd `dirname $0`
 mkdir -p ../data
 sqlite3 ../data/blog.db < ../db/schema.sql
