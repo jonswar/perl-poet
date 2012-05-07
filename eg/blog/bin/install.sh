e0cea8f6bf87617fa322b0d163e20707cdb598fb
Jonathan Swartz <swartz@pobox.com>
2012-05-07 09:48:55 -0700

    fix blog article date/time issue - Pat McNamee <patm@holmescorp.com>

--- eg/blog/bin/install.sh
+++ eg/blog/bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Requires cpanm: http://search.cpan.org/perldoc?App::cpanminus
-cpanm -S --notest Date::Format DBD::SQLite Poet Rose::DB::Object
+cpanm -S --notest DateTime DBD::SQLite Poet Rose::DB::Object
 
 cd `dirname $0`
 mkdir -p ../data
