d80674b780d2042c40223a68613c4397a4dbd438
Jonathan Swartz <swartz@pobox.com>
2012-05-09 04:45:58 -0700

    reduce to one test class

--- lib/Poet/t/NoLog4perl.pm
+++ lib/Poet/t/NoLog4perl.pm
@@ -1,4 +1,4 @@
-# Test Log::Log4perl not being present.
+# Test Log::Log4perl or Log::Any::Adapter::Log4perl not being present.
 #
 package Poet::t::NoLog4perl;
 use Test::Class::Most parent => 'Poet::Test::Class';
@@ -9,7 +9,6 @@ use warnings;
 
 sub test_no_log4perl : Tests {
     my $self       = shift;
-    my $mask       = new Module::Mask('Log::Log4perl');
     my $env        = $self->initialize_temp_env();
     my $error_file = $env->logs_path("poet.log.ERROR");
     ok( -f $error_file, "$error_file exists" );
--- xt/author/NoLog4perl.t
+++ xt/author/NoLog4perl.t
@@ -1,3 +1,5 @@
 #!perl -w
 use Poet::t::NoLog4perl;
+use Module::Mask;
+my $mask = new Module::Mask('Log::Log4perl');
 Poet::t::NoLog4perl->runtests;
